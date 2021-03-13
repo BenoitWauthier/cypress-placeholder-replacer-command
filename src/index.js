925e10a73a608861900e7067594534a64b9f87d8
YBenoit Wauthier <benoit.wauthier@bnpparibasfortis.com>
2021-03-13 22:02:50 +0100

    Remove console log

--- src/index.js
+++ src/index.js
@@ -5,19 +5,14 @@ Cypress.Commands.add('replacePlaceholders', { prevSubject: 'optional' }, (subjec
     var placeholdersIdx = (args.length > (subject ? 0 : 1)) ? args.length-1 : -1
     if (placeholdersIdx > -1) {
         placeholders = args[placeholdersIdx] || {}
-        console.log('-------> placeholders from args ', placeholders)
     }
     const source = subject ? subject : args[0]
-    console.log('-------> ', subject)
-    console.log('-------> ', source)
-    console.log('-------> ', placeholders)
     cy.wrap(placeHolderReplacer(source, placeholders))
 })
 
 Cypress.Commands.add('expectDeepEquals', { prevSubject: false }, (actual, expected, placeholders = {}) => {
-    const actualFormatted = placeHolderReplacer(actual, placeholders)
     const expectedFormatted = placeHolderReplacer(expected, placeholders)
-    cy.expect(castAsJson(actualFormatted)).to.deep.eq(castAsJson(expectedFormatted))
+    cy.expect(castAsJson(actual)).to.deep.eq(castAsJson(expectedFormatted))
 })
 
 const castAsJson = (source) => {
